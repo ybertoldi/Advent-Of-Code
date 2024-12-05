a9a8d25e149fd422b163c428d9a087b00f7d83de
yuri <yuri.bertoldi003@gmail.com>
2024-12-05 10:51:14 -0300

    finished day5 pt1

--- src/year2024/Day5Pt1.java
+++ src/year2024/Day5Pt1.java
@@ -1,65 +1,69 @@
 package year2024;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
 public class Day5Pt1 {
-	public static void main(String[] args) {
-		String input = "47|53\r\n"
-				+ "97|13\r\n"
-				+ "97|61\r\n"
-				+ "97|47\r\n"
-				+ "75|29\r\n"
-				+ "61|13\r\n"
-				+ "75|53\r\n"
-				+ "29|13\r\n"
-				+ "97|29\r\n"
-				+ "53|29\r\n"
-				+ "61|53\r\n"
-				+ "97|53\r\n"
-				+ "61|29\r\n"
-				+ "47|13\r\n"
-				+ "75|47\r\n"
-				+ "97|75\r\n"
-				+ "47|61\r\n"
-				+ "75|61\r\n"
-				+ "47|29\r\n"
-				+ "75|13\r\n"
-				+ "53|13\r\n"
-				+ "\r\n"
-				+ "75,47,61,53,29\r\n"
-				+ "97,61,53,29,13\r\n"
-				+ "75,29,13\r\n"
-				+ "75,97,47,61,53\r\n"
-				+ "61,13,29\r\n"
-				+ "97,13,75,29,47";
+	public static void main(String[] args) throws IOException {
+		File file = new File("C:\\Users\\Yuri\\OneDrive\\Área de Trabalho\\advent of code\\src\\resources\\printQueue.txt");
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		String l;
 		
 		HashMap<String, List<String>> numToList = new HashMap<String, List<String>>();
 		List<String> lines = new ArrayList<String>();
 		boolean changeRead = false;
-		for (String line : input.split("\r\n")) {
-			if (line.equals("")) {
+		
+		while ((l = br.readLine()) != null) {
+			if (l.equals("")) {
 				changeRead = true;
+				continue;
 			}
-			
+
 			if (changeRead) {
-				lines.add(line);
-			}
-			else {
-				String vals[] = line.split("|");
-				
+				lines.add(l);
+			} else {
+				String vals[] = l.split("[|]");
+
 				if (numToList.containsKey(vals[0])) {
 					numToList.get(vals[0]).add(vals[1]);
-				}
-				else {
-					List<String> temp  = new ArrayList<String>();
+				} else {
+					List<String> temp = new ArrayList<String>();
 					temp.add(vals[1]);
 					numToList.put(vals[0], temp);
 				}
 			}
 		}
-		
-		
+
+		int counter = 0;
+		for (String line : lines) {
+			List<String> vals = Arrays.asList(line.split(","));
+			boolean valid = true;
+			for (int i = 0; i < vals.size(); i++) {
+				String val = vals.get(i);
+				if (!numToList.containsKey(val)) {
+					continue;
+				}
+
+				for (String s : numToList.get(val)) {
+					if (vals.contains(s) && vals.indexOf(s) < i) {
+						valid = false;
+						break;
+					}
+				}
+			}
+
+			if (valid) {
+				counter += Integer.valueOf(vals.get(vals.size() / 2));
+			}
+		}
+
+		System.out.println(counter);
 	}
 }
